53749845d3542950adf8d07adb5c245a356588cc
Kevin Moore <kwmoore81@gmail.com>
2015-12-09 12:50:55 -0800

    Fixed lighting

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -2,23 +2,37 @@
 
 public class GameOverManager : MonoBehaviour
 {
-    public PlayerHealth playerHealth;
+    public PlayerHealth playerHealth;       
+    public float restartDelay = 5f;         
 
-
-    Animator anim;
+    Animator anim;                          
+    float restartTimer;                     
 
 
     void Awake()
     {
+        
         anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
+        
         if (playerHealth.currentHealth <= 0)
         {
+           
             anim.SetTrigger("GameOver");
+
+            
+            restartTimer += Time.deltaTime;
+
+           
+            if (restartTimer >= restartDelay)
+            {
+                
+                Application.LoadLevel(Application.loadedLevel);
+            }
         }
     }
 }
